7831a61b24b563367467f62744af041247ba0d7a
Leon <leopard10000@gmail.com>
2022-01-30 21:04:42 +0900

    Vue.js + LaravelでシンプルなSPA構築チュートリアル：LaravelAPI編 - Qiita
    https://qiita.com/minato-naka/items/0e709cb0e6628c82c1c5#%E3%82%BF%E3%82%B9%E3%82%AF%E4%B8%80%E8%A6%A7%E5%8F%96%E5%BE%97api%E5%AE%9F%E8%A3%85

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +22,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{task}', [TaskController::class, 'show']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 
 Route::get('/{any}', function () {
     return view('app');
